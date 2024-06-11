955e84fad2b0e2995849d334d985ddbbac10f7f0
BranDon-Es <brandonthereverse@gmail.com>
2024-06-11 17:52:54 +0300

    added last login for users

--- backend/users/custom_auth_backend.py
+++ backend/users/custom_auth_backend.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.backends import ModelBackend
+from django.utils import timezone
 from .models import CustomUser
-
 
 class CustomUserBackend(ModelBackend):
     def authenticate(self, request, username=None, email=None, password=None, **kwargs):
@@ -18,5 +18,8 @@ class CustomUserBackend(ModelBackend):
             return None
 
         if user.check_password(password):
+            # Update last login timestamp
+            user.last_login = timezone.now()
+            user.save(update_fields=['last_login'])
             return user
         return None
